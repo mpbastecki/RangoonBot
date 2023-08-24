341d18e727116ee21f1c854046b6dd7647b9bdd2
Martin Bastecki <mpbastecki@gmail.com>
2023-08-24 15:49:47 -0400

    Initial commit

---
+++ RangoonBot.py
@@ -0,0 +1,12 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!)
+        
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+        
+        
+client = MyClient()
+client.run('MTE0NDM1MjA5NjYwMzM0MDkzMQ.GhBy6F.jn5NsQbHVLRBf_a9XqKLtquO2m63B7CLkvzClQ')
